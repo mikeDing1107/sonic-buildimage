50b8cd6b29e2b9c546c0cbe5fb6452bce5809614
joshjiang113 <josh_jiang@sdc.sercomm.com>
2026-08-14 02:03:09 +0000

    fix sfp info can't show

--- sc_make.sh
+++ sc_make.sh
@@ -483,6 +483,7 @@ patch_ws()
 	apply_sercomm_patches
 	cp sc_build/kaitian/dts/* platform/marvell-prestera/mrvl-prestera/platform/arm64/common/boot/
 	cp sc_build/kaitian/config/* device/marvell/arm64-marvell_rd98DX45xx_cn9131-r0/rd98DX45xx_cn9131/
+	cp sc_build/kaitian/config/pmon_daemon_control.json device/marvell/arm64-marvell_rd98DX45xx_cn9131-r0/
 	echo > patch_done
     fi
 }
